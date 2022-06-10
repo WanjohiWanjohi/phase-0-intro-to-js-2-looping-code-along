843e593cc443782a01368f6467ee18d31b01f6fa
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-06-10 23:17:40 +0300

    commit tree 531b80a3909de4b397fbc3f920a00d9b742c3c90

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(stringNames, eventName){
+    var messages=[];
+    for(let counter=0; counter<stringNames.length; counter++){
+        var mess = `Thank you, ${stringNames[counter]}, for the wonderful ${eventName} gift!`;
+        messages.push(mess);
+    }
+    return messages;
+}
+function countDown(positiveInteger){
+    let currentDigit = positiveInteger;
+    while ( currentDigit>=0) {
+      console.log(currentDigit);
+      currentDigit -= 1;
+    };
+    
+}
